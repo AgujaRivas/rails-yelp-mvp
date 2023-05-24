80064fc028ca8192636db1646d5a1767a5515295
agustin rivas <agustinmiguelrivas@gmail.com>
2023-05-24 16:55:01 +0200

    This commits our Yelp MVP

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+require 'faker'
+
+puts 'cleaning db..'
+
+Restaurant.destroy_all
+
+puts 'populating db..'
+
+puts 'Creating 100 fake restaurants...'
+100.times do
+  new_restaurant = Restaurant.new(
+    name: Faker::Company.name,
+    address: "#{Faker::Address.street_address}, #{Faker::Address.city}",
+    phone_number: Faker::PhoneNumber.cell_phone,
+    category: ['chinese', 'italian', 'japanese', 'french', 'belgian'].sample
+  )
+  if new_restaurant.save
+    puts "#{new_restaurant.id} was created.."
+  end
+end
+puts 'Finished!'
